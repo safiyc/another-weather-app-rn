03f7f0af7825fea797f44c333a8428592866d277
Safiy Cham <csafiy@yahoo.com>
2020-10-23 08:50:09 -0700

    Ask device for location permission

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'; // location of emulator phone
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    load()
+  }, []);
+
+  async function load() {
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+
+      if (status !== 'granted') {
+        setErrorMessage('Access to Location is needed to run this app.');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+
+      const { latitude, longitude } = location.coords;
+      alert(`latitude: ${latitude}, longitude: ${longitude}`);
+    } catch (error) {
+
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>React Native Weather App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +39,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lime',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    backgroundColor: 'black',
+    padding: 25
+  }
 });
