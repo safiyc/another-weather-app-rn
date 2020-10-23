a3f42fd0b5e09d812eb37aaaa6d8c6f363de8788
Safiy Cham <csafiy@yahoo.com>
2020-10-23 11:47:24 -0700

    Test api requests

--- App.js
+++ App.js
@@ -3,11 +3,17 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location'; // location of emulator phone
 
+import { WEATHER_API_KEY } from 'react-native-dotenv';
+
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitsSystem, setUnitsSystem] = useState('metric');  // default as celsius
 
   useEffect(() => {
-    load()
+    load();
   }, []);
 
   async function load() {
@@ -20,20 +26,44 @@ export default function App() {
       }
 
       const location = await Location.getCurrentPositionAsync();
-
       const { latitude, longitude } = location.coords;
-      alert(`latitude: ${latitude}, longitude: ${longitude}`);
-    } catch (error) {
+      // alert(`latitude: ${latitude}, longitude: ${longitude}`);
 
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`;
+
+      const response = await fetch(weatherUrl);
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setCurrentWeather(result);
+      } else {
+        setErrorMessage(result.message);
+      }
+
+    } catch (error) {
+      setErrorMessage(error.message)
     }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>React Native Weather App</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (currentWeather) {
+    const { main: { temp } } = currentWeather; // 2 level deconstructions of openweather data
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>React Native Weather App</Text>
+        <Text>Temperature: {temp}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>ERROR!</Text>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
